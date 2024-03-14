569c2fedd25d7889fd494abecb95e268490e0487
Josh-Manley <joshmanley45@gmail.com>
2024-03-14 16:00:25 -0600

    created seed file and index file with first choices

--- db/schema.sql
+++ db/schema.sql
@@ -17,8 +17,8 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30)
-  last_name VARCHAR(30)
-  role_id INT
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
   manager_id INT
 );
---
+++ index.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+const fs = require("fs");
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '1antarticA',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
+
+const destinationSelect = {
+  type: 'list',
+  message: 'What would you like to do?',
+  choices: ['View All Departments', 'View All Roles', 'View All Employees',
+  'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+  name: 'destinationSelect'
+}
+
+inquirer.prompt(destinationSelect).then((answer) => {
+  if (answer.destinationSelect === 'View All Departments') {
+    db.query('SELECT * FROM department', function (err, results) {
+      console.log(results);
+    })
+  }
+})
