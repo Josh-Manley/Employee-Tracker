6dfd58978fc33b5b86803670b0e6e169da94da0a
Josh-Manley <joshmanley45@gmail.com>
2024-03-16 17:32:07 -0600

    added query to view all employees selection

--- index.js
+++ index.js
@@ -39,5 +39,23 @@ inquirer.prompt(destinationSelect).then((answer) => {
         console.table(results);
       }
     });
+  } else if (answer.destinationSelect === 'View All Employees') {
+    db.query(`SELECT e.id, 
+    e.first_name, 
+    e.last_name, 
+    r.title, 
+    d.name AS department, 
+    r.salary, 
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee AS e
+    JOIN role AS r ON e.role_id = r.id
+    JOIN department AS d ON r.department_id = d.id
+    LEFT JOIN employee AS m ON e.manager_id = m.id;`, function (err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.table(results);
+      }
+    })
   }
 });
