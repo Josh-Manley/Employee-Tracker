a3833fb3733fd3c5bd435722a64318d934905098
Josh-Manley <joshmanley45@gmail.com>
2024-03-17 16:51:00 -0600

    console log employee table after adding employee

--- index.js
+++ index.js
@@ -252,8 +252,25 @@ function addEmployee() {
                       if (err) {
                         console.log(err);
                       } else {
-                        console.table(results);
-                        selectDestination();
+                        console.log(`${answer.employeeFirstName} ${answer.employeeLastName} added`)
+                        db.query(`SELECT e.id, 
+                                  e.first_name, 
+                                  e.last_name, 
+                                  r.title, 
+                                  d.name AS department, 
+                                  r.salary, 
+                                  CONCAT(m.first_name, ' ', m.last_name) AS manager
+                          FROM employee AS e
+                          JOIN role AS r ON e.role_id = r.id
+                          JOIN department AS d ON r.department_id = d.id
+                          LEFT JOIN employee AS m ON e.manager_id = m.id`, function (err, results) {
+                            if (err) {
+                              console.log(err);
+                            } else {
+                              console.table(results);
+                              selectDestination();
+                            }
+                       })
                       }
                     });
                   }
